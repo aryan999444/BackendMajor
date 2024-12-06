994b0dea9b27932dd5ff0c678e5485ad8a3acead
Aryan Srivastava <aryansrivastava670@gmail.com>
2024-12-06 09:47:59 +0530

    add watch history pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -395,6 +396,63 @@ const getUSerChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler( async (req, res) => {
+    const user = await User.aggregate([
+      {
+        $match:{
+          _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+      },
+      {
+        $lookup:{
+          from: "videos",
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+            {
+              $lookup:{
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                  {
+                    $project: {
+                      fullName: 1,
+                      username: 1,
+                      avatar: 1,
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              $addFields:{
+                owner:{
+                  $first: "$owner"
+                }
+              }
+            }
+          ]
+        }
+      }
+    ])
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          user[0]?.watchHistory,
+          "User watch history fetched successfully"
+        )
+      );
+
+})
+
+
+
 export {
   registerUser,
   loginUser,
@@ -406,4 +464,5 @@ export {
   updateUserAvatar,
   updateUserCoverIamge,
   getUSerChannelProfile,
+  getWatchHistory
 };
