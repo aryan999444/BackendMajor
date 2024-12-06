feef6237a1991155d13a59433feee29452f7a30b
Aryan Srivastava <aryansrivastava670@gmail.com>
2024-12-06 10:00:54 +0530

    Add all User routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import {
-    registerUser,
-    loginUser,
-    logoutUser,
-    refreshAccessToken,
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  uupload,
+  pdateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverIamge,
+  getUSerChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +37,17 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/channel-profile/:userId").get(verifyJWT, getUSerChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
